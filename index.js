03683bdd18dda1e314b50576a6c5abbb03b7075e
celestina01 <celestina.prochilo@meltwater.com>
2020-09-26 16:05:59 -0400

    added bingo files

--- index.js
+++ index.js
@@ -8,3 +8,20 @@ app.get('/', (req, res) => {
 });
 
 app.listen(process.env.PORT || 5000)
+
+var x;
+var table = document.getElementById("myTable");
+if (table != null) {
+  for (var i = 0; i < table.rows.length; i++) {
+    for (var j = 0; j < table.rows[i].cells.length; j++)
+      table.rows[i].cells[j].onclick = function () { getval(this); };
+  }
+}
+function getval(cel) {
+  if (cel.style.color == "") {
+    (cel.style.color = "red")
+  } else if (cel.style.color == "red") {
+    (cel.style.color = "")
+  };
+
+}
