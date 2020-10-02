bd366365c346b5823b96d5d5780c0000c6ac232a
celestina01 <celestina.prochilo@meltwater.com>
2020-10-02 11:34:33 -0400

    fixed uri

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const http = require ('http');        
 const mongoose = require ("mongoose"); 
 const uristring = process.env.KATINI_DB_URI || 'mongodb://localhost/HelloMongoose'
-const theport = process.env.PORT || 5000;
+//const theport = process.env.PORT || 5000;
 
 mongoose.connect(uristring, function (err, res) {
      if (err) {
@@ -22,17 +22,16 @@ app.get('/', (req, res) => {
 app.listen(process.env.PORT || 5000)
 
 var userSchema = new mongoose.Schema({
-     name: {
-       username: { type: String }
-     },
+
+     username: { type: String },
      password: { type: String }
    });
 
 var PUser = mongoose.model('PowerUsers', userSchema);
-...
+
 // Creating one user.
 var johndoe = new PUser ({
-name: { username: 'johndoe' },
+username: 'johndoe',
 password: 'welcome123'
 });
 
