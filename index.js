fbc9fc3abfa4d1a1213f3720350c4dcfee884872
celestina01 <celestina.prochilo@meltwater.com>
2020-10-02 14:26:38 -0400

    reconnecting database

--- index.js
+++ index.js
@@ -17,6 +17,14 @@ app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+mongoose.connect(uristring, function (err, res) {
+     if (err) {
+     console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+     } else {
+     console.log ('Succeeded connected to: ' + uristring);
+     }
+   });
+
 app.get("/", (req, res) => {
   res.json({ message: "API Working" });
 });
@@ -32,13 +40,7 @@ app.listen(PORT, (req, res) => {
   console.log(`Server Started at PORT ${PORT}`);
 });
 
-// mongoose.connect(uristring, function (err, res) {
-//      if (err) {
-//      console.log ('ERROR connecting to: ' + uristring + '. ' + err);
-//      } else {
-//      console.log ('Succeeded connected to: ' + uristring);
-//      }
-//    });
+
 
 
 
