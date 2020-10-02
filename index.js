889c3318571a29ba501d1dd640f663833c9c9908
celestina01 <celestina.prochilo@meltwater.com>
2020-10-02 10:29:25 -0400

    db test fix

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express'); 
 const app = express();
 const http = require('http');
-const mongoose - require("mongoose");
+const mongoose = require("mongoose");
 const uristring = process.env.KATINI_DB_URI || 'mongodb://localhost/HelloMongoose'
 const theport = process.env.PORT || 5000;
 
