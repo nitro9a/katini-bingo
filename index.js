504cc32688b15e82259be47e530fd2c3187d98fb
celestina01 <celestina.prochilo@meltwater.com>
2020-10-02 10:23:38 -0400

    db test

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 const express = require('express'); 
 const app = express();
+const http = require('http');
+const mongoose - require("mongoose");
+const uristring = process.env.KATINI_DB_URI ||
+'mongodb://localhost/HelloMongoose'
+const theport = process.env.PORT || 5000;
+
+mongoose.connect(uristring, function (err, res) {
+     if (err) {
+     console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+     } else {
+     console.log ('Succeeded connected to: ' + uristring);
+     }
+   });
+
 
 app.use(express.static('public'));
 
